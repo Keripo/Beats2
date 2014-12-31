97675c9f69d61ba2ef16148b1f4b305ffb7a3001
Keripo <k.darktiger@gmail.com>
2014-12-31 18:08:50 -0500

    Add MusicPlayerTest

---
+++ beats2/Assets/Scenes/Test/Example/Chaoz Impact - readme.txt
@@ -0,0 +1,19 @@
+Hi there!
+Here it comes, new StepMix entry by Lisek, Chaoz Impact by ParagonX9 grabbed from newgrounds with CC license.
+Graphics are by Najezony, visit his digiart for more of his works ( http://najezony.digart.pl/ ).
+The song is a breakbeat masterpiece by everyone's favourite musican, ParagonX9.
+Just like my previous works, every chart is quality-checked by Chmurek, the StepMix3 winner (the creator of Rockhill).
+
+[Beginner][1]
+Easy breakbeat beginner.
+
+[Light][3]
+Easy patterns, not really tiring.
+
+[Standard][6]
+In this chart I used the steps that match the acid beat, even though it is standard it is pretty challenging.
+
+[Heavy][8]
+A mix of acid and breakbeat style. Many streams and many holds. Also, beware of the mines. Does this chart work? Yeah, pretty much.
+
+
---
+++ beats2/Assets/Scenes/Test/MusicPlayerTest.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Copyright (C) 2015, Philip Peng (Keripo). All rights reserved.
+ * http://beats2.net
+ * The software in this package is published under the terms of the BSD-style license
+ * a copy of which has been included with this distribution in the LICENSE file.
+ */
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Beats2.Audio
+{
+
+	public class MusicPlayerTest : MonoBehaviour
+	{
+		private const string TAG = "MusicPlayerTest";
+
+		public Text togglePlaybackButtonText;
+		public Text playbackStateText;
+		public AudioSource audioSource;
+		public int syncFrameCount;
+
+		public Text logHistoryText;
+		public int logHistorySize;
+
+		private MusicPlayer _player;
+		
+		void Start()
+		{
+			_player = new MusicPlayer(audioSource, syncFrameCount);
+			Logger.SetLogHistory(true);
+			Logger.SetLogHistorySize(logHistorySize);
+		}
+	
+		void Update()
+		{
+			_player.Update();
+			playbackStateText.text = string.Format("Time: {0}", _player.GetTime());
+			logHistoryText.text = Logger.GetLogHistoryString();
+		}
+
+		public void TogglePlayback()
+		{
+			Logger.Debug(TAG, "TogglePlayback");
+			if (_player.IsPlaying()) {
+				_player.Pause();
+				togglePlaybackButtonText.text = "Paused";
+			} else {
+				_player.Play();
+				togglePlaybackButtonText.text = "Playing";
+			}
+		}
+	}
+}
